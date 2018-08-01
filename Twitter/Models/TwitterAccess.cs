45d1685a93e122bcac4b6cc1c8c7a606f7cbc277
matihuf <matihuf@hotmail.com>
2018-08-01 22:33:54 +0200

    add json property

--- Twitter/Models/TwitterAccess.cs
+++ Twitter/Models/TwitterAccess.cs
@@ -1,11 +1,22 @@
-﻿namespace Twitter.Models
+﻿using Newtonsoft.Json;
+
+namespace Twitter.Models
 {
     public class TwitterAccess
     {
+        [JsonProperty("ApiUrl")]
         public string ApiUrl { get; set; }
+
+        [JsonProperty("ConsumerKey")]
         public string ConsumerKey { get; set; }
+
+        [JsonProperty("ConsumerSecret")]
         public string ConsumerSecret { get; set; }
+
+        [JsonProperty("Token")]
         public string Token { get; set; }
+
+        [JsonProperty("TokenSecret")]
         public string TokenSecret { get; set; }
     }
 }
